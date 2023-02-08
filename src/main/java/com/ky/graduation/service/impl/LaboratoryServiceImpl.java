34a0dd450aa96a85e4f258388df34646e5f553f1
ke2fy <3214588281@qq.com>
2023-02-08 19:23:11 +0800

    实验室取消或新增授权人员时更改人员授权状态

--- src/main/java/com/ky/graduation/service/impl/LaboratoryServiceImpl.java
+++ src/main/java/com/ky/graduation/service/impl/LaboratoryServiceImpl.java
@@ -19,6 +19,8 @@ import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务实现类
@@ -90,9 +92,20 @@ public class LaboratoryServiceImpl extends ServiceImpl<LaboratoryMapper, Laborat
     public ResultVo cancelAuthentication(PersonLaboratory personLaboratory) {
         // 邪乎，pId驼峰传不进来，需要pid才能传，我也不知道为什么
         LambdaQueryWrapper<PersonLaboratory> wrapper = Wrappers.lambdaQuery();
-        wrapper.eq(PersonLaboratory::getPId,personLaboratory.getPId()).eq(PersonLaboratory::getLabId,personLaboratory.getLabId());
-        int delete = personLaboratoryMapper.delete(wrapper);
-        if (delete > 0){
+        wrapper.eq(PersonLaboratory::getPId,personLaboratory.getPId());
+        // 首先查询人员已授权的实验室个数
+        List<PersonLaboratory> personLaboratoryList = personLaboratoryMapper.selectList(wrapper);
+        wrapper.eq(PersonLaboratory::getLabId,personLaboratory.getLabId());
+        int deleteNum = personLaboratoryMapper.delete(wrapper);
+        if (deleteNum > 0 && personLaboratoryList.size() > deleteNum){
+            return ResultVo.success();
+        }
+        // 若删除的授权实验室人员授权关系为最后一个授权关系，则将人员实验室状态变为未分配
+        if (deleteNum > 0 && personLaboratoryList.size() == deleteNum){
+            Person person = new Person();
+            person.setId(personLaboratory.getPId());
+            person.setIsDistributed((byte) 0);
+            personMapper.updateById(person);
             return ResultVo.success();
         }
         return ResultVo.error();
@@ -101,12 +114,15 @@ public class LaboratoryServiceImpl extends ServiceImpl<LaboratoryMapper, Laborat
     @Override
     public ResultVo createAuthentication(CreatePersonAuthenticationVO createVo) {
         LambdaQueryWrapper<Person> wrapper = Wrappers.lambdaQuery();
-        // 根据人名和身份证号找人员id
+        // 根据人名和身份证号查询是否存在该人员，并取其id
         wrapper.eq(Person::getName,createVo.getPersonName()).eq(Person::getIdNumber,createVo.getIdNumber());
         Person person = personMapper.selectOne(wrapper);
         if(person == null){
             return ResultVo.error().status(ResultCode.EMPTY_QUERY);
         }
+        // 将人员实验室状态变为已分配
+        person.setIsDistributed((byte) 1);
+        personMapper.updateById(person);
         // 根据人员id和实验室id进行新增操作
         PersonLaboratory personLaboratory = new PersonLaboratory();
         personLaboratory.setPId(person.getId());
